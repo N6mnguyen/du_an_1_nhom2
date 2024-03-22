fd6d1aceec7561be27070501f3ca9745062a05f0
datnmph41088 <150001506+datnmph41088@users.noreply.github.com>
2024-03-22 14:48:01 +0700

    Update danhmucupdate.php

--- admin/view/danhmucupdate.php
+++ admin/view/danhmucupdate.php
@@ -8,18 +8,20 @@ if (is_array($dm) && (count($dm) > 0)) {
         Sửa Danh Mục
     </h3>
 
-    <form class="addPro" action="admin.php?pg=dmupdate" onsubmit="return send()" method="POST" enctype="multipart/form-data">
+    <form class="addPro" action="admin.php?pg=dmupdate" onsubmit="return send()" method="POST"
+        enctype="multipart/form-data">
         <div class="form-group">
             <label for="name">Tên Danh Mục:</label>
-            <input type="text" class="form-control" name="namedm" id="name" value="<?= $name ?>" placeholder="Nhập tên Danh Mục">
+            <input type="text" class="form-control" name="namedm" id="name" value="<?= $name ?>"
+                placeholder="Nhập tên Danh Mục">
             <p style="margin-top: 5px; text-align: left; color:red;" class="err_danhmuc"></p>
             <input type="hidden" name="id" value="<?= $id ?>">
         </div>
         <div class="form-group">
             <label for="name">Ẩn hiện:</label>
-            <input type="checkbox" value="1" name="trangthai" <?=$home==1?'checked':''?>>
+            <input type="checkbox" value="1" name="trangthai" <?= $home == 1 ? 'checked' : '' ?>>
             <label for="name">Hot:</label>
-            <input type="checkbox" value="1" name="trangthai1" <?=$stt==1?'checked':''?>>
+            <input type="checkbox" value="1" name="trangthai1" <?= $stt == 1 ? 'checked' : '' ?>>
         </div>
         <div class="form-group">
             <button type="submit" name="adddm" class="btn btn-primary">Sửa Danh Mục</button>
