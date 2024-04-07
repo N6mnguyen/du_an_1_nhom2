382ffebd1c2448b43746f06a3358171b79040723
nhvan04 <163501664+nhvan04@users.noreply.github.com>
2024-04-07 17:53:14 +0700

    Update convnpay.php

--- dao/convnpay.php
+++ dao/convnpay.php
@@ -1,1 +1,19 @@
+<?php
 
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+  
+$vnp_TmnCode = "2A19BVCP"; //Mã định danh merchant kết nối (Terminal Id)
+$vnp_HashSecret = "LAMYWMYRBEOMWTJDVVAFRZTZGMTYEHSS"; //Secret key
+$vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+$vnp_Returnurl = "http://localhost/duan/duan1/du_an_mau_2023/index.php?pg=billconfirm";
+$vnp_apiUrl = "http://sandbox.vnpayment.vn/merchant_webapi/merchant.html";
+$apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
+//Config input format
+//Expire
+$startTime = date("YmdHis");
+$expire = date('YmdHis',strtotime('+15 minutes',strtotime($startTime)));
